5e3b4edc56f6ddfb3b4aef03b9ac27a5a5d9f227
snowcha <hello@snowcha.com>
2023-01-28 14:12:57 -0700

    added 3 files html, css, and js

---
+++ hiragana.js
@@ -0,0 +1,345 @@
+ const audio = new Audio();
+  audio.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/あ-2464554.mp3";
+
+  const audio1 = new Audio();
+  audio1.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/い-2464554.mp3";
+
+  const audio2 = new Audio();
+  audio2.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/う-2464554.mp3";
+
+  const audio3 = new Audio();
+  audio3.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/え-2464554.mp3";
+
+  const audio4 = new Audio();
+  audio4.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/お-2464554.mp3";
+
+  const audio5 = new Audio();
+  audio5.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/か-2464554.mp3";
+
+  const audio6 = new Audio();
+  audio6.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/き-2464554.mp3";
+
+  const audio7 = new Audio();
+  audio7.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/く-2464554.mp3";
+
+  const audio8 = new Audio();
+  audio8.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/け-2464555.mp3";
+
+  const audio9 = new Audio();
+  audio9.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/こ-2464555.mp3";
+
+  const audio10 = new Audio();
+  audio10.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/さ-2464555.mp3";
+
+  const audio11 = new Audio();
+  audio11.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/し-2464555.mp3";
+
+  const audio12 = new Audio();
+  audio12.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/す-2464555.mp3";
+
+  const audio13 = new Audio();
+  audio13.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/せ-2464555.mp3";
+
+  const audio14 = new Audio();
+  audio14.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/そ-2464555.mp3";
+
+  const audio15 = new Audio();
+  audio15.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/た-2464555.mp3";
+
+  const audio161 = new Audio();
+  audio161.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ち-2464556.mp3";
+
+  const audio17 = new Audio();
+  audio17.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/つ-2464556.mp3";
+
+  const audio18 = new Audio();
+  audio18.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/て-2464556.mp3";
+
+  const audio19 = new Audio();
+  audio19.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/と-2464556.mp3";
+
+  const audio21 = new Audio();
+  audio21.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/な-2464556.mp3";
+
+  const audio22 = new Audio();
+  audio22.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/に-2464556.mp3";
+
+  const audio23 = new Audio();
+  audio23.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ぬ-2464556.mp3";
+
+  const audio24 = new Audio();
+  audio24.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ね-2464556.mp3";
+
+  const audio25 = new Audio();
+  audio25.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/の-2464557.mp3";
+
+  const audio27 = new Audio();
+  audio27.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/は-2464557.mp3";
+
+  const audio28 = new Audio();
+  audio28.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ひ-2464557.mp3";
+
+  const audio29 = new Audio();
+  audio29.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ふ-2464557.mp3";
+
+  const audio30 = new Audio();
+  audio30.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/へ-2464557.mp3";
+
+  const audio31 = new Audio();
+  audio31.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ほ-2464557.mp3";
+
+  const audio33 = new Audio();
+  audio33.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ま-2464557.mp3";
+
+  const audio34 = new Audio();
+  audio34.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/み-2464557.mp3";
+
+  const audio35 = new Audio();
+  audio35.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/む-2464558.mp3";
+
+  const audio36 = new Audio();
+  audio36.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/め-2464558.mp3";
+
+  const audio37 = new Audio();
+  audio37.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/も-2464558.mp3";
+
+  const audio39 = new Audio();
+  audio39.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/や-2464558.mp3";
+
+  const audio40 = new Audio();
+  audio40.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ゆ-2464558.mp3";
+
+  const audio41 = new Audio();
+  audio41.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/よ-2464558.mp3";
+
+  const audio43 = new Audio();
+  audio43.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ら-2464558.mp3";
+
+  const audio44 = new Audio();
+  audio44.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/り-2464558.mp3";
+
+  const audio45 = new Audio();
+  audio45.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/る-2465912.mp3";
+
+  const audio46 = new Audio();
+  audio46.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/れ-2465912.mp3";
+
+  const audio47 = new Audio();
+  audio47.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ろ-2465912.mp3";
+
+  const audio49 = new Audio();
+  audio49.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/わ-2465967.mp3";
+
+  const audio50 = new Audio();
+  audio50.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/を-2465967.mp3";
+
+  const audio51 = new Audio();
+  audio51.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ん-2465967.mp3";
+
+  const audio52 = new Audio();
+  audio52.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ga-2464548.mp3";
+
+  const audio53 = new Audio();
+  audio53.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/gi-2464548.mp3";
+
+  const audio54 = new Audio();
+  audio54.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/gu-2464548.mp3";
+
+  const audio55 = new Audio();
+  audio55.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ge-2464548.mp3";
+
+  const audio56 = new Audio();
+  audio56.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/go-2464548.mp3";
+
+  const audio57 = new Audio();
+  audio57.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/za-2464553.mp3";
+
+  const audio58 = new Audio();
+  audio58.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ji-2466174.mp3";
+
+  const audio59 = new Audio();
+  audio59.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/zu-2464553.mp3";
+
+  const audio60 = new Audio();
+  audio60.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ze-2464553.mp3";
+
+  const audio61 = new Audio();
+  audio61.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/zo-2464553.mp3";
+
+  const audio62 = new Audio();
+  audio62.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/da-2464547.mp3";
+
+  const audio63 = new Audio();
+  audio63.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/di-2464547.mp3";
+
+  const audio64 = new Audio();
+  audio64.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/du-2464547.mp3";
+
+  const audio65 = new Audio();
+  audio65.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/de-2464547.mp3";
+
+  const audio66 = new Audio();
+  audio66.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/do-2464547.mp3";
+
+  const audio67 = new Audio();
+  audio67.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ba-2464545.mp3";
+
+  const audio68 = new Audio();
+  audio68.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/bi-2464546.mp3";
+
+  const audio69 = new Audio();
+  audio69.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/bu-2464546.mp3";
+
+  const audio70 = new Audio();
+  audio70.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/be-2464546.mp3";
+
+  const audio71 = new Audio();
+  audio71.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/bo-2464546.mp3";
+
+  const audio72 = new Audio();
+  audio72.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/va-2464553.mp3";
+
+  const audio73 = new Audio();
+  audio73.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/vi-2464553.mp3";
+
+  const audio74 = new Audio();
+  audio74.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/v-2464553.mp3";
+
+  const audio75 = new Audio();
+  audio75.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ve-2464553.mp3";
+
+  const audio76 = new Audio();
+  audio76.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/vo-2466576.mp3";
+
+  const audio98 = new Audio();
+  audio98.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pa-2464550.mp3";
+
+  const audio99 = new Audio();
+  audio99.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pi-2464550.mp3";
+
+  const audio100 = new Audio();
+  audio100.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pu-2464551.mp3";
+
+  const audio101 = new Audio();
+  audio101.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pe-2464550.mp3";
+
+  const audio102 = new Audio();
+  audio102.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/po-2464551.mp3";
+
+  const audio107 = new Audio();
+  audio107.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/kya-2464549.mp3";
+
+  const audio108 = new Audio();
+  audio108.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/kyu-2464550.mp3";
+
+  const audio109 = new Audio();
+  audio109.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/kyo-2464550.mp3";
+
+  const audio89 = new Audio();
+  audio89.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/sha-2466778.mp3";
+
+  const audio90 = new Audio();
+  audio90.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/shu-2464552.mp3";
+
+  const audio91 = new Audio();
+  audio91.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/sho-2501082.mp3";
+
+  const audio110 = new Audio();
+  audio110.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/cha-2464546.mp3";
+
+  const audio111 = new Audio();
+  audio111.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/chu-2464547.mp3";
+
+  const audio112 = new Audio();
+  audio112.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/cho-2464547.mp3";
+
+  const audio114 = new Audio();
+  audio114.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/nya-2464550.mp3";
+
+  const audio115 = new Audio();
+  audio115.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/nyu-2464550.mp3";
+
+  const audio116 = new Audio();
+  audio116.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/nyo-2464550.mp3";
+
+
+  const audio92 = new Audio();
+  audio92.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/hya-2464549.mp3";
+
+  const audio93 = new Audio();
+  audio93.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/hyu-2464549.mp3";
+
+  const audio94 = new Audio();
+  audio94.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/hyo-2464549.mp3";
+
+  const audio1180 = new Audio();
+  audio1180.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/mya-2501678.mp3";
+
+  const audio1190 = new Audio();
+  audio1190.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/myu-2501678.mp3";
+
+  const audio117 = new Audio();
+  audio117.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/myo-2501678.mp3";
+
+  const audio95 = new Audio();
+  audio95.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/rya-2464551.mp3";
+
+  const audio96 = new Audio();
+  audio96.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ryu-2464552.mp3";
+
+  const audio97 = new Audio();
+  audio97.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ryo-2464551.mp3";
+
+  const audio77 = new Audio();
+  audio77.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/gya-2464548.mp3";
+
+  const audio78 = new Audio();
+  audio78.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/gyu-2464548.mp3";
+
+  const audio79 = new Audio();
+  audio79.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/gyo-2464548.mp3";
+
+  const audio80 = new Audio();
+  audio80.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ja-2464549.mp3";
+
+  const audio81 = new Audio();
+  audio81.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ju-2464549.mp3";
+
+  const audio82 = new Audio();
+  audio82.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/jo-2464549.mp3";
+
+  const audio180 = new Audio();
+  audio180.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ja-2464549.mp3";
+
+  const audio181 = new Audio();
+  audio181.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/ju-2464549.mp3";
+
+  const audio182 = new Audio();
+  audio182.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/jo-2464549.mp3";
+
+  const audio83 = new Audio();
+  audio83.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/bya-2464546.mp3";
+
+  const audio84 = new Audio();
+  audio84.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/byu-2464546.mp3";
+
+  const audio85 = new Audio();
+  audio85.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/byo-2464546.mp3";
+
+  const audio183 = new Audio();
+  audio183.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/bya-2464546.mp3";
+
+  const audio184 = new Audio();
+  audio184.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/byu-2464546.mp3";
+
+  const audio185 = new Audio();
+  audio185.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/byo-2464546.mp3";
+
+  const audio103 = new Audio();
+  audio103.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pya-2464551.mp3";
+
+  const audio104 = new Audio();
+  audio104.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pyu-2464551.mp3";
+
+  const audio105 = new Audio();
+  audio105.src = "https://content.app-sources.com/s/60388582350729606/uploads/audio/pyo-2464551.mp3";
